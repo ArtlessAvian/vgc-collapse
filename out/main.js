26fc208034146a4bb45fe4ed2b036a9014acc933
Steven Welden <swelden7@gmail.com>
2020-02-02 01:53:00 -0800

    Fixed card number

--- out/main.js
+++ out/main.js
@@ -4,12 +4,23 @@ Promise.all(Model.promises)
     main = new Collapser();
     $('document').ready(function () {
         console.log("Dom Ready!");
-        $("#back-1").click(function () { main.backstep(); });
+        $("#back-1").click(function () {
+            main.backstep();
+        });
         $("#forward-1").click(function () { main.step(); });
         $("#forward-5").click(function () { main.fiveStep(); });
         $("select").on("select2:selecting", function (event) {
             var pokemon = event.params.args.data.text;
-            var cardNum = (this.id).charAt((this.id).length - 1);
+            var idLen = (this.id).length;
+            var cardNum;
+            if (idLen > 15) {
+                cardNum = parseInt((this.id).charAt((this.id).length - 3));
+            }
+            else {
+                cardNum = parseInt((this.id).charAt((this.id).length - 1));
+            }
+            console.log(pokemon);
+            console.log(cardNum);
         });
     });
     function updateCard(cardNum, matrix) {
--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,29 @@ Promise.all(Model.promises)
     $('document').ready(function() { 
         console.log("Dom Ready!")
         
-        $("#back-1").click(function() {main.backstep()});
+        $("#back-1").click(function()
+        {
+            main.backstep()
+        });
         $("#forward-1").click(function() {main.step()});
         $("#forward-5").click(function() {main.fiveStep()});
         // $("select/").select(function(event) {})
         $("select").on("select2:selecting", function(event) {
             // console.log(event, this);
             let pokemon = event.params.args.data.text;
-            let cardNum = (this.id).charAt((this.id).length-1);
-            // console.log(pokemon);
-            // console.log(cardNum);
+            let idLen = (this.id).length;
+            let cardNum : number;
+
+            if (idLen > 15)
+            {
+                cardNum = parseInt((this.id).charAt((this.id).length-3));
+            } else
+            {
+                cardNum = parseInt((this.id).charAt((this.id).length-1));
+            }
+
+            console.log(pokemon);
+            console.log(cardNum);
         }); //main.update
     });
 
