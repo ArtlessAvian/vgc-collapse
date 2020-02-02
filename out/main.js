9ba27f588c3c0f4fc9fab080e6c56af8255db736
Ryan <artlessavian@gmail.com>
2020-02-01 23:23:34 -0800

    main improvements?

--- out/main.js
+++ out/main.js
@@ -2,8 +2,14 @@ var main;
 Promise.all(Model.promises)
     .then(function () {
     main = new Collapser();
-    $("#forward-1").click(function () { main.step(); });
-    $("#forward-5").click(function () { main.step(); });
+    $('document').ready(function () {
+        console.log("Dom Ready!");
+        $("#forward-1").click(function () { main.step(); });
+        $("#forward-5").click(function () { main.fiveStep(); });
+        $(".select2").on("select2:selecting", function (event) {
+            console.log(event);
+        });
+    });
     Dropdown.createPokemonNameDropdown(main.pos.matrix[0]);
-    console.log("Ready!");
+    console.log("Main Ready!");
 });
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,22 @@ let main : Collapser;
 Promise.all(Model.promises)
 .then(function() {
     main = new Collapser();
-    
-    $("#forward-1").click(function() {main.step()});
-    $("#forward-5").click(function() {main.step()});
-    
+
+    $('document').ready(function() { 
+        console.log("Dom Ready!")
+
+        $("#forward-1").click(function() {main.step()});
+        $("#forward-5").click(function() {main.fiveStep()});
+        $(".select2").on("select2:selecting", function(event) {
+            console.log(event);
+            // let name = event.currentTarget.innerText;
+            // console.log(name)
+            // alert(name);
+        }); //main.update
+    });
+
+
     Dropdown.createPokemonNameDropdown(main.pos.matrix[0])
 
-    console.log("Ready!");
+    console.log("Main Ready!");
 })
